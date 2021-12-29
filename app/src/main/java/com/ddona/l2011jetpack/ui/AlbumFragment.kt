9d42bd564e266c90bf630587d4ac0bb1316fb8cf
DoanPT1 <doanptand@gmail.com>
2021-12-29 20:45:03 +0700

    Share data between view model with view model provider

--- app/src/main/java/com/ddona/l2011jetpack/ui/AlbumFragment.kt
+++ app/src/main/java/com/ddona/l2011jetpack/ui/AlbumFragment.kt
@@ -5,17 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.ddona.l2011jetpack.databinding.FragmentAlbumBinding
 import com.ddona.l2011jetpack.databinding.FragmentSongBinding
+import com.ddona.l2011jetpack.vm.MusicViewModel
 
 class AlbumFragment : Fragment() {
-    private lateinit var binging: FragmentAlbumBinding
+    private lateinit var binding: FragmentAlbumBinding
+    private lateinit var viewModel: MusicViewModel
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binging = FragmentAlbumBinding.inflate(layoutInflater)
-        return binging.root
+        binding = FragmentAlbumBinding.inflate(layoutInflater)
+//        viewModel = ViewModelProvider(this).get(MusicViewModel::class.java)
+        viewModel = ViewModelProvider(requireActivity()).get(MusicViewModel::class.java)
+        binding.btnUp.setOnClickListener { viewModel.increaseNumber() }
+        viewModel.number.observe(viewLifecycleOwner, {
+            binding.tvNumber.text = it.toString()
+        })
+        return binding.root
     }
 }
