aed52dcd78fb86f852b90c582e3f153961b86963
DoanPT1 <doanptand@gmail.com>
2022-01-12 20:32:04 +0700

    using data binding

---
+++ app/src/main/java/com/ddona/l2011jetpack/BindingActivity.kt
@@ -0,0 +1,32 @@
+package com.ddona.l2011jetpack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import com.bumptech.glide.Glide
+import com.ddona.l2011jetpack.databinding.ActivityBindingBinding
+import com.ddona.l2011jetpack.vm.BindingViewModel
+
+class BindingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBindingBinding
+    private val viewModel: BindingViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBindingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.vm = viewModel
+        binding.lifecycleOwner = this
+
+//        binding.btnLoad.setOnClickListener {
+//            Glide.with(this)
+//                .load(binding.edtLink.text.toString())
+//                .into(binding.imgAvatar)
+//        }
+//        viewModel.link.observe(this, {
+//            Log.d("doanpt", it)
+//        })
+    }
+}
