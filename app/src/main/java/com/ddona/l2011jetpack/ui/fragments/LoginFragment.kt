8591517128882ca93d647604b854cfddae003f9d
DoanPT1 <doanptand@gmail.com>
2022-01-19 21:14:31 +0700

    handle deeplink with navigation

--- app/src/main/java/com/ddona/l2011jetpack/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/ddona/l2011jetpack/ui/fragments/LoginFragment.kt
@@ -6,17 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.ddona.l2011jetpack.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
     private lateinit var binding: FragmentLoginBinding
+    private val args: LoginFragmentArgs by navArgs()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
-
+        val username = args.username
+        binding.edtUsername.setText(username)
         binding.btnLogin.setOnClickListener {
             val user = binding.edtUsername.text.toString()
             val pass = binding.edtPassword.text.toString()
